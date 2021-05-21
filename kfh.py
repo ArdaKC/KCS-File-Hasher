803b4b8320ed5994d210798483e8357e9a58b1ff
i5-7200u <81111892+i5-7200u@users.noreply.github.com>
2021-05-22 00:27:30 +0300

    Add files via upload

---
+++ kfh.py
@@ -0,0 +1,60 @@
+import hashlib
+
+print("KCS File Hasher 1.0.0-^")
+print("Which language? / Hangi dil? \n 1-Türkçe\n 2-English")
+dil = int(input("Select language / Dil seçin:"))
+if dil == 1:
+ hoh = input("Konum girin:")
+ dh = open(hoh, "rb")
+ hooh = dh.read()
+ print(" 1-SHA1 \n 2-SHA256 \n 3-SHA384 \n 4-SHA512 \n 5-MD5 \n 6-SHA224")
+ ahoh = input("Hash seçin:")
+ if ahoh == "1":
+   sha1 = hashlib.sha1(hooh).hexdigest()
+   print("Dosyanin SHA1 Hash değeri = " + sha1)
+ elif ahoh == "2":
+   sha2 = hashlib.sha256(hooh).hexdigest()
+   print("Dosyanin SHA256 Hash değeri = " + sha2)
+ elif ahoh == "3":
+   sha3 = hashlib.sha384(hooh).hexdigest()
+   print("Dosyanin SHA384 Hash değeri = " + sha3)
+ elif ahoh == "4":
+   sha5 = hashlib.sha512(hooh).hexdigest()
+   print("Dosyanin SHA512 Hash değeri = " + sha5)
+ elif ahoh == "5":
+   md5 = hashlib.md5(hooh).hexdigest()
+   print("Dosyanin MD5 Hash değeri = " + md5)
+ elif ahoh == "6":
+   sha224 = hashlib.sha224(hooh).hexdigest()
+   print("Dosyanin SHA224 Hash değeri = " + sha224)
+ else:
+   sha2 = hashlib.sha256(hooh).hexdigest()
+   print("Dosyanin SHA256 Hash değeri = " + sha2)
+
+elif dil == 2:
+ hoh = input("Specify the location:")
+ dh = open(hoh, "rb")
+ hooh = dh.read()
+ print(" 1-SHA1 \n 2-SHA256 \n 3-SHA384 \n 4-SHA512 \n 5-MD5 \n 6-SHA224")
+ ahoh = input("Select Hash:")
+ if ahoh == "1":
+   sha1 = hashlib.sha1(hooh).hexdigest()
+   print("File SHA1 Hash value = " + sha1)
+ elif ahoh == "2":
+   sha2 = hashlib.sha256(hooh).hexdigest()
+   print("File SHA256 Hash value = " + sha2)
+ elif ahoh == "3":
+   sha3 = hashlib.sha384(hooh).hexdigest()
+   print("File SHA384 Hash value = " + sha3)
+ elif ahoh == "4":
+   sha5 = hashlib.sha512(hooh).hexdigest()
+   print("File SHA512 Hash value = " + sha5)
+ elif ahoh == "5":
+   md5 = hashlib.md5(hooh).hexdigest()
+   print("File MD5 Hash value = " + md5)
+ elif ahoh == "6":
+   sha224 = hashlib.sha224(hooh).hexdigest()
+   print("File SHA224 Hash value = " + sha224)
+ else:
+   sha2 = hashlib.sha256(hooh).hexdigest()
+   print("File SHA256 Hash value = " + sha2)
